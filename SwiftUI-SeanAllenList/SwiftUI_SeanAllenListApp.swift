be326e61a5d9d96d812a0ad52616092b9ae211e0
mostafa ayman <mostafaaymanpro@gmail.com>
2021-09-22 05:19:43 +0200

    Initial Commit

---
+++ SwiftUI-SeanAllenList/SwiftUI_SeanAllenListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SeanAllenListApp.swift
+//  SwiftUI-SeanAllenList
+//
+//  Created by Mostafa Ayman on 22/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SeanAllenListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
